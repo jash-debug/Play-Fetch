abd523039d6d6f0cf31af68220a6cb318310af00
jash-debug <jash-debug@github.com>
2022-09-04 17:02:33 +0300

    Add Solution requirements for Challenge 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    //Create a time delay variable
+    private float timeDelay = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Increase Time delay after every second
+        timeDelay += Time.deltaTime;
+
+        // On spacebar press, send dog and if time is more that 2 seconds delay
+        if (Input.GetKeyDown(KeyCode.Space) && timeDelay > 2  )
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeDelay = 0;
+        }
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        InvokeRepeating("SpawnRandomBall", startDelay, SpawnInterval());
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        
+        //Invoke("SpawnRandomBall", Random.Range(3.0f, 5.0f));
+
+        // Generate random ball index and random spawn position
+        int ballIndex = (Random.Range(0, ballPrefabs.Length));
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        
+    }
+
+    // Spawn Interval of 3 to 5 seconds for the ball prefabs
+     float SpawnInterval ()
+    {
+        return (Random.Range(3.0f,5.0f));
+    }
+
+}
